54d210f5f377b694523c2d7b7dd0e9f8a128b9ff
Leah Victoria <systemgalaction@gmail.com>
2024-12-21 16:07:01 -0600

    Update logic.js

--- stlayer_data/logic.js
+++ stlayer_data/logic.js
@@ -11,19 +11,8 @@ if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; re
   let opener = _____WB$wombat$assign$function_____("opener");
 
 // probably a better way to do this but who cares LOL
-var imageURLs = [ "images/img1.png", "images/img2.jpg", "images/img3.jpg", 
-				  "images/img4.png", "images/img5.png", "images/img6.jpg", 
-				  "images/img7.jpg", "images/img8.png", "images/img9.png", 
-				  "images/img10.jpg", "images/img11.png", "images/img12.png", 
-				  "images/img13.png", "images/img14.png", "images/img15.jpg", 
-				  "images/img16.png", "images/img17.jpg", "images/img18.jpg", 
-				  "images/img19.png", "images/img20.jpg", "images/img21.png",
-				  "images/img22.jpg", "images/img23.jpg", "images/img24.png",
-				  "images/img25.png"];
-var audioURLs = [ "sounds/1.mp3", "sounds/2.mp3", "sounds/3.mp3", 
-				  "sounds/4.mp3", "sounds/5.mp3", "sounds/6.mp3", 
-				  "sounds/7.mp3", "sounds/8.mp3", "sounds/9.mp3", 
-				  "sounds/10.mp3", "sounds/11.mp3", "sounds/12.mp3", ];
+var imageURLs = [ "images/img22.png";
+var audioURLs = [ "sounds/6.mp3"];
 function GetImage() {
 	var img = '<img src=\"';
 	var randomIndex = Math.floor(Math.random() * imageURLs.length);
