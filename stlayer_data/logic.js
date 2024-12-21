0059d593cb8554a9fccdad2393a027947ed4d705
Leah Victoria <systemgalaction@gmail.com>
2024-12-21 16:28:19 -0600

    Update logic.js

--- stlayer_data/logic.js
+++ stlayer_data/logic.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 // probably a better way to do this but who cares LOL
 var imageURLs = [ "stlayer_data/img22.jpg";
 var audioURLs = [ "stlayer_data/6.mp3"];
@@ -32,23 +20,3 @@ function GetAudio() {
 }
 
 }
-/*
-     FILE ARCHIVED ON 11:30:05 Jun 07, 2020 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 21:17:55 Dec 21, 2024.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.642
-  exclusion.robots: 0.025
-  exclusion.robots.policy: 0.012
-  esindex: 0.013
-  cdx.remote: 16.394
-  LoadShardBlock: 189.574 (3)
-  PetaboxLoader3.datanode: 137.627 (4)
-  PetaboxLoader3.resolve: 125.962 (2)
-  load_resource: 140.523
-*/
